0754192f68ae379c9d38eefa536bf4ced5e8c5fd
Digital Dragon <rrajendra@yahoo.com>
2026-02-05 19:36:35 -0500

    Add thread pool demo

--- ParallelismAndConcurrency/ThreadPools/ThreadPoolsDemo.java
+++ ParallelismAndConcurrency/ThreadPools/ThreadPoolsDemo.java
@@ -1,6 +1,6 @@
 import static java.util.concurrent.Executors.newFixedThreadPool;
 
-public class ThreadPoolDemo {
+public class ThreadPoolsDemo {
     public static void main(String[] args) {
         var threadPool = newFixedThreadPool(3);
         for (int i = 0; i < 10; i++) {
